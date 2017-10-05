9deec8b7360671be8d87c2f565399bb560a50b38
Chris Reid <cr01927@georgiasouthern.edu>
2017-10-05 13:50:14 -0400

    Filled error_correction and pos_evaluation

--- src/pos_estimator2/error_correction_node.py
+++ src/pos_estimator2/error_correction_node.py
@@ -4,12 +4,38 @@
 #   in order to reclassify data for retraining.
 
 
-def error_correction_node():
-
+class ErrorCorrection():
+    """ Corrects errors by acquiring the evaluated position, calculating the error, 
+        and distributing the error over multiple training locations """
+        
+    def __init__(self):
+        rospy.init_node('error_correction')
+        
+        # Setup services
+        self.srv_train_cnns = rospy.ServiceProxy('train_cnns', TrainCNNs)
+        self.srv_get_position = rospy.ServiceProxy('get_current_position', EstimatePosition)
+        self.rospy.wait_for_service('train_cnns')
+        self.rospy.wait_for_service('get_current_position')
+        self.srv_correct_errors = rospy.Service('correct_errors', pos_estimator2.srv.CorrectErrors, correct_errors)
+        
+    def correct_errors(pos):
+        """ Calculates errors and renames folders based on the given current position """
+        
+        # First get the current position
+        pos = self.srv_get_position()
+        # Calculate error (if reference is (0,0,0) then error should just be the value of our current position
+        # Except for theta, which could be different (by a multiple of 90 deg)
+        
+        # Figure out which folders need to have their error corrected
+        
+        # Rename those folders
+        
+        # Instruct the training_node to train/retrain the CNNs
+        self.srv_train_cnns()
 
 
 if __name__ == "__main__":
     try:
-	    error_correction_node()
+	    ErrorCorrection()
 	except rospy.ROSInterruptException:
 	    pass
--- src/pos_estimator2/pos_evaluation_node.py
+++ src/pos_estimator2/pos_evaluation_node.py
@@ -3,14 +3,48 @@
 # This node evaluates our current position based initially on odometry, later
 #  a combination of odometry, an RGB CNN, and a depth CNN
 
+import rospy
 
-def pos_evaluation_node():
+from sensor_msgs import Odometry, Image
+from geometry_msgs import Pose2D
 
 
+
+
+class PosEvaluation():
+    """ Evaluates the current position of the robot via odometry and CNNs """
+    def __init__(self):
+        
+        rospy.init_node('pos_evaluation_node')
+
+        # Setup service
+        self.srv_evaluate_position = rospy.Service('get_current_position', pos_estimator2.srv.EstimatePosition, get_current_position)
+    
+    
+    def get_current_position():
+    
+        # Get odometry-based position
+        odom = rospy.wait_for_message(rospy.get_param('odometry_topic', Odometry)
+        # Calculate euler angles from the odometry quaternion
+        euler = tf.transformations.euler_from_quaternion(odom.pose.quaternion)
+        # Copy x and y from the odometry pose into our Pose2D, along with theta
+        odom_position = Pose2D()
+        odom_position.x = odom.pose.pose.position.x
+        odom_position.y = odom.pose.pose.position.y
+        odom_position.theta = euler[2]
+        
+        # If the RGB and Depth CNN exist, evaluate position from them as well
+        
+        # Combine the three estimated positions into our best guess
+        
+        # Return that position
+        current_position = CurrentPosition()
+        # Fill current_position much like odom_position was filled earlier
+        return current_position
 
 
 if __name__ == "__main__":
     try:
-	    pos_evaluation_node()
+	    PosEvaluation()
 	except rospy.ROSInterruptException:
 	    pass
