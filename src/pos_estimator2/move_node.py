d35702d3a0c790afe8eff33aeb46bf5772451de3
Chris Reid <cr01927@georgiasouthern.edu>
2017-10-10 14:43:29 -0400

    Added in coord transforms

--- src/pos_estimator2/move_node.py
+++ src/pos_estimator2/move_node.py
@@ -15,6 +15,10 @@ class MoveNode():
     def __init__(self):
     
         self.coords = rospy.get_param('coords')
+        tmp = [None]*len(coords)
+        for i in coords:
+            tmp[int(i)-1] = coords[i]
+        self.coords = tmp
         self.num_pts = len(coords)
         self.cur_node = 0 # Start at origin
         
@@ -35,6 +39,18 @@ class MoveNode():
         
         if not self.action_client.send_goal_and_wait(action_goal, rospy.Duration(50.0) == GoalStatus.SUCCEEDED:
             rospy.loginfo('Call to turtlebot_actions server failed')
+            
+        # We got to our goal
+        self.cur_node += 1
+        if self.cur_node +1 > self.num_pts:
+            # This means current node is origin, next node is undefined
+            # So calculate the next set of nodes
+            self.cur_node = 0
+            theta = get_theta()
+            self.coords = transform_coords(theta, self.coords)
+            # And reset our cur_node to 0
+            self.cur_node=0
+            
         
     def get_theta():
         # Get odometry-based position
@@ -93,6 +109,14 @@ class MoveNode():
         
     def convert_coords(coord):
         return float(coord['x']), float(coord['y'])
+        
+    def transform_coords(coords, theta):
+        new_coords = [None]*length(coords)
+        for i in coords:
+            new_coords[i]['x'] = math.cos(theta)*coords[i]['x'] - math.sin(theta)*coords[i]['y']
+            new_coords[i]['y'] = math.sin(theta)*coords[i]['x'] - math.cos(theta)*coords[i]['y']
+        return new_coords
+        
      
 
 
